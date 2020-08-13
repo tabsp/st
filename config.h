17610e8690e9693fe4f868535329d15c79c7ad1f
tabsp <tabsp@qq.com>
2020-08-13 14:31:56 +0800

    set font size

--- config.h
+++ config.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:pixelsize=24:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=20:antialias=true:autohint=true";
 /* Spare fonts */
-static char *font2[] = { "JoyPixels:pixelsize=24:antialias=true:autohint=true" }; 
+static char *font2[] = { "JoyPixels:pixelsize=20:antialias=true:autohint=true" }; 
 
 static int borderpx = 2;
 
